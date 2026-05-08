1d303fe411760ba7284c099d2a4e7389c74acf7e
AngryDK <bragedgm@gmail.com>
2026-05-08 11:57:57 -0500

    fixed errors + added mappings

---
+++ dev_name_mappings.py
@@ -0,0 +1,29 @@
+hero_name_mapping = {
+"Warmaster": "Helena",
+"Sune": "Xavian",
+"Lisa": "Aeona",
+"Ink": "Tariq",
+"Gunde": "Gunde (WIP)",
+"Bowguy": "Elarion",
+"Firemage": "Ardeos",
+"Mara": "Mara",
+"Meiko": "Meiko",
+"Mosse": "Sylvie",
+"Rime": "Rime",
+"Vigor": "Vigour",
+}
+
+zone_name_mapping = {
+"DesertDunes": "Empyrean Sands",
+"DemonicRitual": "Heart of Tuzari",
+"ElfMines": "Godfall Quarry",
+"FrozenKingdom": "Cithrel's Fall",
+"IceCave": "Wyrmheart",
+"MythicForest": "Everdawn Grove", 
+"NightPatrol": "Stormwatch",
+"PirateRaid": "Wraithtide Vault",
+"ShipGraveyard": "Sailor's Abyss",
+"SilkenHollow": "Silken Hollow",
+"UrrakMarkets": "Urrak Markets",
+"VikingVillage": "Ransack of Drakheim",
+}
